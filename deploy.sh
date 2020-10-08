f62ca02c413fdce18cb6cfc61fc6136a77ad06d2
namnguyen191 <hoangnamnguyen191@gmail.com>
2020-10-07 21:52:34 -0400

    Fix image naming issue

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t namnguyen191/multi-client:latest -t namnguyen191/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t namnguyen191/multi-server:latest -t namnguyen191/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t namnguyen191/multi-worker:latest -t namnguyen191/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push namnguyen191/multi-client:latest
-docker push namnguyen191/multi-server:latest
-docker push namnguyen191/multi-worker:latest
-docker push namnguyen191/multi-client:$SHA
-docker push namnguyen191/multi-server:$SHA
-docker push namnguyen191/multi-worker:$SHA
+docker build -t namng191/multi-client:latest -t namng191/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t namng191/multi-server:latest -t namng191/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t namng191/multi-worker:latest -t namng191/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push namng191/multi-client:latest
+docker push namng191/multi-server:latest
+docker push namng191/multi-worker:latest
+docker push namng191/multi-client:$SHA
+docker push namng191/multi-server:$SHA
+docker push namng191/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=namnguyen191/multi-server:$SHA
-kubectl set image deployments/client-deployment server=namnguyen191/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=namnguyen191/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=namng191/multi-server:$SHA
+kubectl set image deployments/client-deployment server=namng191/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=namng191/multi-worker:$SHA
