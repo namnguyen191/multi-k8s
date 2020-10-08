8ef9118fccdc84c573a11e37af58bf42de6612b0
namnguyen191 <hoangnamnguyen191@gmail.com>
2020-10-07 21:50:47 -0400

    Try trigger redeploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 docker build -t namnguyen191/multi-client:latest -t namnguyen191/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t namnguyen191/multi-server:latest -t namnguyen191/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t namnguyen191/multi-worker:latest -t namnguyen191/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-# We dont have to log in since we already logged in inside travis
 docker push namnguyen191/multi-client:latest
 docker push namnguyen191/multi-server:latest
 docker push namnguyen191/multi-worker:latest
-#TEST
-
 docker push namnguyen191/multi-client:$SHA
 docker push namnguyen191/multi-server:$SHA
 docker push namnguyen191/multi-worker:$SHA
-
 kubectl apply -f k8s
-
 kubectl set image deployments/server-deployment server=namnguyen191/multi-server:$SHA
 kubectl set image deployments/client-deployment server=namnguyen191/multi-client:$SHA
 kubectl set image deployments/worker-deployment server=namnguyen191/multi-worker:$SHA
